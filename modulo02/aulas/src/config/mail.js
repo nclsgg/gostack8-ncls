9c6dc6a16f88d5681092fe9bbac6c84abf12d341
nclsgg <nicolasguadagno@gmail.com>
2019-09-11 19:01:30 -0300

    Trabalhando com Mailtrap e handlebars

---
+++ modulo02/aulas/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '31e17fcf9d6620',
+    pass: '55c2ba3cf8b0e1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
